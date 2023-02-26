6bebc9fc1cee3e0dff172606d1cd5dc6178bbdf3
Ivan Maruzhenko <ivanmaruzhenko@gmail.com>
2023-02-26 13:14:49 +0200

    Commit to get latest update

--- App.py
+++ App.py
@@ -6,13 +6,42 @@
 # version 0.1
 
 
-def schedule_for_the_day():
-    pass
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = 'BOT TOKEN HERE'
+
+# Configure logging
+
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+
+bot = Bot(token=API_TOKEN)
+
+dp = Dispatcher(bot)
 
 
-def schedule_for_the_week():
-    pass
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+
+    This handler will be called when user sends `/start` or `/help` command
+
+    """
+
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
 
-def all_schedule():
-    pass
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
