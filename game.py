deb60f0a988924b474160c18800612b6712b7425
Ivan Skorobagatko <ivanskorobagatko@gmail.com>
2023-05-25 02:04:51 +0200

    Connected Game to Bot | Reworked Report Bug Setting

--- game.py
+++ game.py
@@ -6,42 +6,57 @@
 """
 
 from asyncio import sleep
-import telegram
-from telegram.ext import ContextTypes, ApplicationBuilder, CommandHandler, CallbackQueryHandler, ConversationHandler, \
-    MessageHandler, filters
-import logging
-from DataBase.db_function_game import user_check, update_score_by_user, add_new_gamer, update_games_by_user, \
+from telegram import Update
+from telegram.ext import ContextTypes, ConversationHandler
+from telegram.constants import ParseMode
+from loger_config import logger
+from Database.db_function_game import (
+    user_check, update_score_by_user, add_new_gamer, update_games_by_user,
     score_by_gamer, games_by_gamer, top_gamers
+)
+
+ADD_PLAYER = chr(13)
+MAIN_GAME = chr(14)
 
 
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-#     level=logging.INFO
-# )
+async def game_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Entry Point for Game"""
+    user = update.message.from_user
 
-async def start(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
-    name = update.effective_user.id
-    if user_check(name):
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text="Привіт, граємо ?")
-        return 2
-    else:
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text="Привіт, бачу, ви тут вперше. Як вас звати ?")
-        return 1
+    logger.info(f"User: {user.username}, user_id: {user.id}. The user has started game.")
+
+    if user_check(user.id):
+        await context.bot.send_message(chat_id=user.id, text="Привіт, граємо? (так/ні)")
+
+        return MAIN_GAME
+
+    await context.bot.send_message(chat_id=user.id, text="Привіт, бачу, ви тут вперше. Як вас звати?")
+
+    return ADD_PLAYER
 
 
-async def add_gamer(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+async def add_player(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Add Player to Database"""
     user_name = update.message.text
+    user = update.message.from_user
     add_new_gamer(update.effective_user.id, user_name)
+
+    logger.info(f"User: {user.username}, user_id: {user.id}. The user has been added to game database.")
+
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="Ви успішно додані списку гравців, тепер ви можете відслідковувати свій рейтинг серед всіх участників гри."
-                                        f" {user_name}, починаємо грати ?")
-    return 2
+                                   text=f"Ви успішно додані до списку гравців, тепер ви можете \
+                                          відслідковувати свій рейтинг серед всіх участників гри."
+                                        f"\n{user_name}, починаємо грати? (так/ні)")
+    return MAIN_GAME
 
 
-async def dice_game(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+async def dice_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Main Game (DICE)"""
     user_answer = update.message.text
+    user = update.message.from_user
+
+    logger.info(f"User: {user.username}, user_id: {user.id}. The user has started the game of dice.")
+
     if user_answer.lower() == 'так':
 
         async def roll_dice(chat_id, message) -> int:
@@ -49,76 +64,60 @@ async def dice_game(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE)
             data = await context.bot.send_dice(chat_id=chat_id)
             return data["dice"]["value"]
 
-        data_user = await roll_dice(update.effective_chat.id,'Ви підкидаєте кубик')
+        data_user = await roll_dice(update.effective_chat.id, 'Ви підкидаєте кубик')
         await sleep(5)
-        data_bot = await roll_dice(update.effective_chat.id,'Бот підкидає кубик')
+        data_bot = await roll_dice(update.effective_chat.id, 'Бот підкидає кубик')
         await sleep(4)
+
         if data_bot > data_user:
-            await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text='Ви програли')
-            update_games_by_user(update.effective_user.id, games_by_gamer(update.effective_user.id))
-            games = games_by_gamer(update.effective_user.id)
-            await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text=f'Кількість виграшів: {score_by_gamer(update.effective_user.id)}\nКількість спроб: {games}')
-
-
+            await context.bot.send_message(chat_id=update.effective_chat.id, text='Ви програли')
 
         elif data_user > data_bot:
-            await  context.bot.send_message(chat_id=update.effective_chat.id,
-                                            text='Ви виграли')
-            update_score_by_user(update.effective_user.id,  score_by_gamer(update.effective_user.id))
-            update_games_by_user(update.effective_user.id, games_by_gamer(update.effective_user.id))
-            games = games_by_gamer(update.effective_user.id)
-            await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text=f'Кількість виграшів: {score_by_gamer(update.effective_user.id)}\nКількість спроб: {games}')
-
+            await context.bot.send_message(chat_id=update.effective_chat.id, text='Ви виграли')
+            update_score_by_user(update.effective_user.id, score_by_gamer(update.effective_user.id) + 1)
 
         else:
-            await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text='Нічия')
+            await context.bot.send_message(chat_id=update.effective_chat.id, text='Нічия')
 
-            update_games_by_user(update.effective_user.id, games_by_gamer(update.effective_user.id))
-            games = games_by_gamer(update.effective_user.id)
-            await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text=f'Кількість виграшів: {score_by_gamer(update.effective_user.id)}\nКількість спроб: {games}')
-        await sleep(3)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text='Граємо ще раз ? (так/ні)')
-        return 2
-    else:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text='Тоді йди на пари')
+        update_games_by_user(update.effective_user.id, games_by_gamer(update.effective_user.id) + 1)
+        games = games_by_gamer(update.effective_user.id)
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f'Кількість виграшів: {score_by_gamer(update.effective_user.id)}\n'
+                                            f'Кількість спроб: {games}')
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Граємо ще раз? (так/ні)')
+        return MAIN_GAME
+
+    elif user_answer.lower() == 'ні':
+
+        logger.info(f"User: {user.username}, user_id: {user.id}. The user has ended the game.")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='До нової зустрічі!')
         return ConversationHandler.END
 
 
-async def gamers_top(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+async def top_players(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Top 10 Players in Database"""
+    user = update.message.from_user
+
+    logger.info(f"User: {user.username}, user_id: {user.id}. The user requested list of top players.")
+
     top_list = top_gamers()
-    str = 'Топ 10 гравців:'
+    top10 = '```\nТоп 10 гравців:'
+
     for i in range(len(top_list[0])):
-        str += f"\n{i + 1}) {top_list[1][i]}     -    {top_list[0][i]}"
+        top10 += "\n{0:>2}) {2:<10}  -  {1}".format(i + 1, top_list[0][i], top_list[1][i][:10:])
 
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text=str)
+    top10 += '```'
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=top10, parse_mode=ParseMode.MARKDOWN)
+    await context.bot.send_message(chat_id=update.effective_chat.id, parse_mode=ParseMode.HTML,
+                                   text='Щоб розпочати гру, напишіть <b>/start_game</b>')
 
 
-async def stop(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="До наступної гри !")
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """End Game"""
+    user = update.message.from_user
+
+    logger.info(f"User: {user.username}, user_id: {user.id}. The user has ended the game.")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="До наступної гри!")
     return ConversationHandler.END
-
-
-conv_handler = ConversationHandler(entry_points=[CommandHandler('start', start)],
-                                   states={1: [MessageHandler(filters.TEXT, add_gamer)],
-                                           2: [MessageHandler(filters.TEXT, dice_game)]},
-
-                                   fallbacks=[CommandHandler('stop', stop)])
-
-# if __name__ == '__main__':
-#     application = ApplicationBuilder().token('token').build()
-#
-#     start_handler = CommandHandler('start', start)
-#     top_handler = CommandHandler('top', gamers_top)
-#     stop_handler = CommandHandler('stop', stop)
-#     application.add_handler(conv_handler)
-#     application.add_handler(start_handler)
-#     application.add_handler(top_handler)
-#     application.add_handler(stop_handler)
-#     application.run_polling()
